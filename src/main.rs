b852574e8a0b6003cbc9c4e2f9192762870f17e1
sn2234 <sn2234@gmail.com>
2019-05-18 12:56:43 +0300

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 #![allow(non_snake_case,dead_code,non_camel_case_types,non_upper_case_globals)]
 
+#[macro_use]
+extern crate log;
+extern crate log4rs;
+
 mod locations;
 
 fn main() {
+    initLogging();
+
+    info!("Starting application");
+
     println!("Hello, world!");
+}
+
+fn initLogging() {
+    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
 }
